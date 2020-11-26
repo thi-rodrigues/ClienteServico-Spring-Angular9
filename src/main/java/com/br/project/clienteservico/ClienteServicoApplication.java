3404c7ecb59ee08774b076b0579cd513e4e1f704
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2020-11-26 01:10:28 -0300

    Projeto ClienteServico-Spring-Angular9 iniciado

---
+++ src/main/java/com/br/project/clienteservico/ClienteServicoApplication.java
@@ -0,0 +1,13 @@
+package com.br.project.clienteservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteServicoApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/br/project/clienteservico/ClienteServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.project.clienteservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
