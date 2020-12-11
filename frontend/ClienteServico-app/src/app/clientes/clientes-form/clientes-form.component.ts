7a5fe931e78c83a9e10e89dd694801a022242d27
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2020-12-11 01:22:20 -0300

    Mostrando mensagem de Errors e Success

--- frontend/ClienteServico-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ frontend/ClienteServico-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -11,6 +11,8 @@ import { ClientesService } from '../../clientes.service';
 export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
+  success: boolean = false;
+  errors: String[];
 
   constructor( private service : ClientesService ) { 
     this.cliente = new Cliente();
@@ -21,9 +23,13 @@ export class ClientesFormComponent implements OnInit {
 
   onSubmit(){
     this.service
-      .salvar(this.cliente)
-      .subscribe( response => {
-        console.log(response);
+      .salvar(this.cliente).subscribe( response => {
+        this.success = true;
+        this.errors = null;
+        this.cliente = response;
+      }, errorResponse => {
+        this.success = false;
+        this.errors = errorResponse.error.errors;
       })
   }
 }
