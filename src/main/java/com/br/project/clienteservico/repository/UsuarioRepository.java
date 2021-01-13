b7045366648a557c8eba281fdec220b9b7322b30
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2021-01-13 00:35:05 -0300

    Autenticacao com usuarios da base de dados

--- src/main/java/com/br/project/clienteservico/repository/UsuarioRepository.java
+++ src/main/java/com/br/project/clienteservico/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.br.project.clienteservico.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.br.project.clienteservico.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 
+	Optional<Usuario> findByUsername(String username);
 }
---
+++ src/main/java/com/br/project/clienteservico/service/UsuarioService.java
@@ -0,0 +1,32 @@
+package com.br.project.clienteservico.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.br.project.clienteservico.model.Usuario;
+import com.br.project.clienteservico.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = usuarioRepository
+				.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Login não econtrado."));
+		
+		return User
+				.builder()
+				.username(usuario.getUsername())
+				.password(usuario.getPassword())
+				.roles("USER")
+				.build();
+	}
+}
