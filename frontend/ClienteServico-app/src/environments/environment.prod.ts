b2e0e233614334f199b85497402b548c91dd1491
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2021-03-28 16:50:45 -0300

    deployed the application on heroku

--- frontend/ClienteServico-app/src/environments/environment.prod.ts
+++ frontend/ClienteServico-app/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURLBase: 'https://cliente-service-back-end.herokuapp.com/',
+  clientId: 'my-angular-app',
+  clientSecret: '@MY-JWT-SECRET',
+  obterTokenUrl:  '/oauth/token'
 };
